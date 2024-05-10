43e90afe0a6d79d297905e5dd1fb65cd321a0adb
MISTER-X <134623251+Sorry124@users.noreply.github.com>
2024-05-10 15:54:50 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Check if the files exist
 if [ -f "crt" ] && [ -f "shaista_sub" ]; then
     # Set executable permissions on the files
-    chmod +x crt shaista_sub
+    sudo chmod +x crt shaista_sub
     # Move the files to /usr/local/bin
     sudo mv crt shaista_sub /usr/local/bin/
     echo "Files moved successfully."
