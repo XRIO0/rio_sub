539fb11210162181f4a5021ad0de083d41227367
MISTER-X <134623251+Sorry124@users.noreply.github.com>
2024-05-10 15:54:15 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Check if the script is run with root privileges
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run this script as root."
-    exit 1
-fi
-
 # Check if the files exist
-if [ ! -f "crt" ] || [ ! -f "shaista_sub" ]; then
-    echo "Error: Files 'crt' and 'shaista_sub' not found."
-    exit 1
+if [ -f "crt" ] && [ -f "shaista_sub" ]; then
+    # Set executable permissions on the files
+    chmod +x crt shaista_sub
+    # Move the files to /usr/local/bin
+    sudo mv crt shaista_sub /usr/local/bin/
+    echo "Files moved successfully."
+else
+    echo "Error: One or both files not found."
 fi
-
-# Move files to /usr/local/bin
-mv crt shaista_sub /usr/local/bin/
-
-echo "Files moved successfully to /usr/local/bin."
