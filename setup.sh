7f0c1d6e02a04b0b667deb4b295a75d84b055293
MISTER-X <134623251+Sorry124@users.noreply.github.com>
2024-05-10 16:27:48 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [ -f "crt" ] && [ -f "shaista_sub" ]; then
     # Set executable permissions on the files
     chmod +x crt shaista_sub
     # Move the files to /usr/local/bin
-    sudo mv crt shaista_sub /usr/local/bin/
+    sudo cp crt shaista_sub /usr/local/bin/
     echo "Files moved successfully."
 else
     echo "Error: One or both files not found."
