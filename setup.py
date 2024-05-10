1e07b413f3b1e506d60543229ed68ba8af98a2ae
MISTER-X <134623251+Sorry124@users.noreply.github.com>
2024-05-10 16:54:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 import shutil
 import os
 
-# Get the current working directory
 source_directory = os.getcwd()
 
-# Define the destination directory where you want to move the binary files
 destination_directory = "/usr/local/bin"
 
-# List of binary files to be moved
 binary_files = ["crt", "shaista_sub"]
 
-# Loop through each binary file and move it to the destination directory
 for file_name in binary_files:
     source_path = os.path.join(source_directory, file_name)
     destination_path = os.path.join(destination_directory, file_name)
@@ -19,8 +15,7 @@ for file_name in binary_files:
         shutil.move(source_path, destination_path)
         print(f"File '{file_name}' moved successfully to '{destination_directory}'.")
         
-        # Add permission to the moved binary file
-        os.chmod(destination_path, 0o755)  # 0o755 corresponds to rwxr-xr-x permission
+        os.chmod(destination_path, 0o755) 
         
         print(f"Permission set for file '{file_name}' in '{destination_directory}'.")
     except FileNotFoundError:
